fec673149abfc1e85bd86fe078488a933cb237bb
Vasanthkumar M <vasanth23402@gmail.com>
2025-02-05 18:56:50 +0530

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavBar } from '../components/NavBar'
+import { Landing } from '../components/Landing'
+
+export const Home = () => {
+  return (
+    <div>
+        <NavBar />
+        <Landing />
+    </div>
+  )
+}
