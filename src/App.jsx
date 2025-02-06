1a218d13c0e207ef837f548d62700be7c8b5d392
Afrina Afrin <afrinaafrin1033@gmail.com>
2025-02-06 18:09:17 +0530

    I have created the navlinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 import { Home } from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+import { About } from "./pages/About"
+import { Contact } from "./pages/Contact"
+import { Register } from "./pages/Register"
 
 function App() {
 
   return (
     <>
-      <Home /> 
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+      </Routes>
     </>
   )
 }
--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+import logo from "../assets/Destinova.png";
+import "../styles/NavBar.css"
+import {useState} from 'react'
 
 export const NavBar = () => {
+  const [display, setDisplay] = useState('false')
+  const showMenu = () => {
+    setDisplay(!display)
+    let nav = document.getElementById('nav').style.paddingBottom = display? '0px' : '10px'
+    let res = document.getElementsById('nav-links')
+    for(let i=0; i<res.length; i++) {
+        res[i].style.display = display? 'flex' : 'none'
+    }
+  }
+
   return (
-    <div>
-        
-    </div>
+    <nav>
+      <div>
+        <img src={logo} alt="Logo" className="project-logo" />
+      </div>
+      <div onClick={showMenu} id='menu_bar'>
+        <div>
+            <div className='menu'></div>
+            <div className='menu'></div>
+            <div className='menu'></div>
+        </div>
+        <div>
+            <span>MENU</span>
+        </div>
+      </div>
+
+      <div id='nav-links'>
+            <NavLink className="nav-link" to='/'>Home</NavLink>
+            <NavLink className="nav-link" to='/about'>About</NavLink>
+            <NavLink className="nav-link" to='/contact'>Contact</NavLink>
+            <NavLink className="nav-link" to='/register'>Register</NavLink>
+      </div>
+    </nav>
   )
 }
