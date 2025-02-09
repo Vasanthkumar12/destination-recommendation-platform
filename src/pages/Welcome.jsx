1f0cb9ca2490b1663e54b9b997057fe74834bbd7
Vasanthkumar M <vasanth23402@gmail.com>
2025-02-09 20:19:11 +0530

    deployed our project on netlify

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -22,7 +22,7 @@ export const Welcome = () => {
       setBgImage(images[index])
       setPadLeft(paddingsLeft[index])
       setPadTop(paddingsTop[index])
-    }, 3000)
+    }, 4000)
     return () => clearInterval(interval)
   }, [])
 
